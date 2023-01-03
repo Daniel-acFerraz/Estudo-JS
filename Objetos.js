c93901ce30d4f2561ac6623fba0b97c1642bb7bd
Daniel-acFerraz <daniel.albinocf@gmail.com>
2023-01-03 16:30:39 -0300

    Create Objetos.js

---
+++ Objetos.js
@@ -0,0 +1,17 @@
+let pokemonsTypes = {
+    charizard: 'fire',
+    blastoise: 'water',
+    venossaur: 'plant',
+    pikachu: 'thunder',
+    raichu: 'thunder',
+    charmander: 'fire',
+    squirtle: 'water',
+    bulbassaur: 'plant'
+}
+
+let typesPokemon = {
+    fire: ['charmander', 'charizard'],
+    water: ['squirtle', 'blastoize'],
+    plant: ['bulbassaur', 'venossaur'],
+    thunder: ['pikachu', 'raichu']
+}
