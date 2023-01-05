4bd90ae0db2b9aa0ed1c4135fa11b199c51eeaf5
Daniel-acFerraz <daniel.albinocf@gmail.com>
2023-01-05 15:53:45 -0300

    Update Objetos.js

--- Objetos.js
+++ Objetos.js
@@ -9,9 +9,24 @@ let pokemonsTypes = {
     bulbassaur: 'plant'
 }
 
-let typesPokemon = {
-    fire: ['charmander', 'charizard'],
-    water: ['squirtle', 'blastoize'],
-    plant: ['bulbassaur', 'venossaur'],
-    thunder: ['pikachu', 'raichu']
-}
+const typesPokemon = {pokemons: [
+    {name: 'charmander', type: 'fire'},
+    {name: 'squirtle', type: 'water'},
+    {name: 'bulbassaur', type: 'plant'},
+    {name: 'pikachu', type: 'thunder'},
+  ]}
+  
+  let keys = Object.keys(pokemonsTypes);
+  //console.log("Keys are ", keys)
+  let values = Object.values(pokemonsTypes);
+  //console.log("Values are", values)
+  let entries = Object.entries(pokemonsTypes);
+  //console.log("The pokemon", entries[5][0], 'is', entries[5][1])
+  
+  let size = typesPokemon['pokemons'].length
+  console.log(size)
+  let poke = typesPokemon['pokemons']
+  //console.log(poke)
+  for (let i = 0; i < poke.length; i+=1) {
+    console.log(poke[i]);
+  }
